027db537df1c4f89ec3dd14eaaee1eee915bf055
yuwi_guthib <hueysumanting24@gmail.com>
2025-11-16 23:44:51 +0800

    Update navigation links to use base path

--- includes/nav.php
+++ includes/nav.php
@@ -1,17 +1,22 @@
+<?php
+// Determine base path for assets and links
+$base_path = (strpos($_SERVER['PHP_SELF'], 'pages/') !== false) ? '../' : '';
+?>
+
 <nav class="main-nav">
     <div class="left-section">
-        <img src="<?php echo $logo; ?>" alt="BrewHaven Cafe Logo" class="logo debug">
+        <img src="<?php echo $base_path; ?>assets/img/logo.png" alt="BrewHaven Cafe Logo" class="logo">
         <span class="site-name">BrewHaven Cafe PH</span>
         <div class="divider"></div>
     </div>
 
     <div class="right-section">
         <ul class="nav-links">
-            <li><a href="../index.php">Home</a></li>
-            <li><a href="../pages/contact.html">Contacts</a></li>
-            <li><a href="../pages/about.html">About us</a></li>
-            <li><a href="../pages/faq.html">FAQs</a></li>
+            <li><a href="<?php echo $base_path; ?>index.php">Home</a></li>
+            <li><a href="<?php echo $base_path; ?>pages/menu.php">Menu</a></li>
+            <li><a href="<?php echo $base_path; ?>pages/contact.html">Contacts</a></li>
+            <li><a href="<?php echo $base_path; ?>pages/about.html">About us</a></li>
+            <li><a href="<?php echo $base_path; ?>pages/faq.html">FAQs</a></li>
         </ul>
-
     </div>
 </nav>
