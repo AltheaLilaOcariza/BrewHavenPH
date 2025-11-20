731d70fc1f153abeaaf74262839f7ebaef90d0ca
yuwi_guthib <hueysumanting24@gmail.com>
2025-11-20 13:40:35 +0800

    Updated footer.php for the index.php to access the login form for admin to access the dashboard

--- includes/footer.php
+++ includes/footer.php
@@ -9,12 +9,11 @@
         <div class="footer-section">
             <h4>Quick Links</h4>
             <ul>
-                <li><a href="<?php echo $base_path; ?>index.php">Home</a></li>
-                <li><a href="<?php echo $base_path; ?>pages/menu.php">Menu</a></li>
-                <li><a href="<?php echo $base_path; ?>pages/about.html">About Us</a></li>
-                <li><a href="<?php echo $base_path; ?>pages/contact.html">Contact</a></li>
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="menu.php">Menu</a></li>
+                <li><a href="about.html">About Us</a></li>
+                <li><a href="contact.html">Contact</a></li>
             </ul>
-        </ul>
         </div>
         
         <div class="footer-section">
@@ -23,17 +22,11 @@
             <p>📞 (02) 8123-4567</p>
             <p>✉️ hello@brewhaven.com</p>
         </div>
-        
+    </div>
     
     <div class="footer-bottom">
-        <p>&copy; 2025 BrewHaven Cafe PH. All rights reserved.</p>
+        <p>&copy; 2025 BrewHaven Cafe PH. All rights reserved. 
+           | <small><a href="/BrewHavenCafe/admin/login.php" style="color: #FFD88F;">Staff Login</a></small>
+        </p>
     </div>
 </footer>
-<?php
-// Print any extra JS files enqueued by pages. Pages can set $extra_js = ['path/to/file.js', ...]
-if (isset($extra_js) && is_array($extra_js)) {
-    foreach ($extra_js as $js_file) {
-        echo '<script src="' . htmlspecialchars($js_file, ENT_QUOTES) . '"></script>' . PHP_EOL;
-    }
-}
-?>
